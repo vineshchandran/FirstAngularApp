67f8ce45146bdfe4b773e53c631fff6ac7539c97
vinesh <vineshchandran.cad@gmail.com>
2020-05-20 09:46:19 +0100

    Initial Commit

---
+++ DatingApp/Data/ApplicationDataContext.cs
@@ -0,0 +1,20 @@
+﻿using DatingApp.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApp.Data
+{
+    public class ApplicationDataContext : DbContext
+    {
+        //Contructor
+        public ApplicationDataContext(DbContextOptions<ApplicationDataContext> options) : base(options)
+        {
+
+        }
+        // Add the models created here.
+        public DbSet<Value> Values { get; set; }
+    }
+}
